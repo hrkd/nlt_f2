619d6bcb8fe575ed085568cd203a1721fbf5934d
hrkd <hiroki@hrkd.net>
2016-04-04 21:42:36 +0900

    .

--- js-source/app.js
+++ js-source/app.js
@@ -1,7 +1,18 @@
-;(function(){
+;(function(APP){
+'use strict';
+var Observer = require('./util/Observer');
+var Class = function(s) {
+};
+
+//shortcut
+var p = Class.prototype;
+p.APP = {
+  state: {},
+  NAME : 'this app name'
+};
+p.observer = new Observer();
+
 module.exports = (function() {
-  return {
-    NAME: 'this app name'
-  };
+  return Class;
 })();
 })();
