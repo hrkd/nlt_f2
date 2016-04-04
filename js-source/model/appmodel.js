ce7b42087c25166535b0cc773ca0c849387a6220
hrkd <hiroki@hrkd.net>
2016-04-04 21:42:19 +0900

    データ更新の実装をまとめる

--- js-source/model/appmodel.js
+++ js-source/model/appmodel.js
@@ -1,27 +1,36 @@
-;(function(){
-var $ = require('jquery');
-var Class;
+(function(){
+'use strict';
+var util = require('util');
+var Super = require('../app');
+var that;
 
-function Class() {
-  console.log('const super class.');
-}
+//shortcut
+var Class = function(s) {
+  that = this;
+  console.log('const AppModel class.');
 
-//public
-Class.prototype.myname = function() {
-  return getName();
+  //データが取得されたら実行
+  that.observer.on('replaceData',function(){
+    that.observer.trigger('render');
+  });
+};
+util.inherits(Class,Super);
+
+//shortcut
+var p = Class.prototype;
+
+//public method
+p.replaceData = function(data){
+  that.APP.state.apiData = data;
+  that.observer.trigger('replaceData');
 };
 
-//public
-Class.prototype.apiclient = function() {
-  return $.ajax('/');
-};
 
-//private
-var getName = function(){
-  return 'sato';
+//private method
+function privateMethod(){
 }
 
 module.exports = (function() {
-  return Class
+  return Class;
 })();
 })();
